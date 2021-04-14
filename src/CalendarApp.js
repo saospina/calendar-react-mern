911a61500438d32a9e6642b0362a4881ad117b5e
saospina <sergos0617@gmail.com>
2021-04-14 12:30:32 -0500

    setting up the application

---
+++ src/CalendarApp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+export const CalendarApp = () => {
+    return (
+        <div>
+            <h1>Calendar App</h1>
+        </div>
+    )
+}
